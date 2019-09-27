0cec394bb2f409e441443038cee900b6b865d8d1
Edouard Leurent <eleurent@gmail.com>
2019-09-27 18:08:04 +0200

    Better version of hide.js
    - Keep the paragraph / linebreaks structure instead of using divs
    - Simply wrap lines and words into spans at initialization
    - Choose a new ordering when hiding at level 0
    - The spans do not contain spaces which means that we can still
    see the sizes of words after hiding.

--- assets/js/hide.js
+++ assets/js/hide.js
@@ -1,4 +1,34 @@
-var hidden_index, max_index;
+var hidden_index = 0, max_index = 0;
+
+function wrapLines(div) {
+    div.children('p').each(function() {
+        $(this).contents().filter(function() {
+            return this.nodeType == 3;
+        }).wrap('<span class="line">');
+    });
+}
+
+function wrapWords() {
+    $('.line').each(function() {
+        var words = $(this).html().split(' ').filter(x => x),
+            result = [];
+        for (var i = 0; i < words.length; i++ ) {
+            result[i] = '<span class="word">' + words[i] + '</span>';
+        }
+        $(this).html(result.join(' '));
+    });
+}
+
+function orderWords() {
+    $('.line').each(function() {
+        var words = $(this).find('.word');
+        max_index = Math.max(words.length, max_index);
+        var order = randomArray(0, words.length)
+        words.each(function(index) {
+            $(this).attr("order", order[index]);
+        });
+    });
+}
 
 function randomArray(min, max) {
   return (new Array(max-min))
@@ -7,67 +37,29 @@ function randomArray(min, max) {
     .sort().map(function(v) { return v[1]; });
 }
 
-function extractWords(line) {
-    var words = line.split(' ').filter(x => x);
-    var spans = []
-    var order = randomArray(0, words.length)
-    max_index = Math.max(max_index, words.length)
-    for(var w=0; w<words.length; w++) {
-        var span = $('<span class="word" order=' + order[w] +'></span>')
-        span.append(words[w] + ' ')
-        spans.push(span)
-    }
-    return spans
-}
-
-function extractLines(div) {
-    var lines = div.text().split('\n');
-    div.html('');
-    for(var l=0; l<lines.length; l++) {
-        if (lines[l]) {
-            var line_div = $('<div class="line">');
-            var words = extractWords(lines[l])
-            for (var w=0; w<words.length; w++)
-                line_div.append(words[w])
-            div.append(line_div);
-        }
-        if (l>0) {
-            div.append($('<br />'));
-        }
-    }
-}
-
-function reorderLines() {
-    $('.line').each(function() {
-        var words = $(this).find('.word');
-        var order = randomArray(0, words.length)
-        words.each(function(index) {
-            $(this).attr("order", order[index]);
-        });
-    });
-}
 
 jQuery(document).ready(function(){
-    // Initialization
-    hidden_index = max_index = 0;
     // Extract words and lines
     var div = $('#text-content');
-    extractLines(div)
+    wrapLines(div)
+    wrapWords()
+
     // Hide words and increase index
     $('#hide').bind('click', function(){
+        // New shuffling
+        if (hidden_index == 0)
+            orderWords()
         $('.word[order=' + hidden_index + ']').each(function (index, value) {
             $(this).addClass("hidden");
         });
         hidden_index = Math.min(hidden_index + 1, max_index)
     });
+
     // Show words and decrease index
     $('#show').bind('click', function(){
         hidden_index = Math.max(hidden_index - 1, 0);
         $('.word[order=' + hidden_index + ']').each(function (index, value) {
             $(this).removeClass("hidden");
         });
-        // Scramble the lines hiding order again when every word is revealed
-        if (hidden_index == 0)
-            reorderLines()
     });
 });
